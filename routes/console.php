3e318d378737e3bb23972c5e71a212faf8487094
frmhfrd <fahrimuhamadfirdaus17@gmail.com>
2026-06-03 09:16:15 +0700

    feat: add cron job to transition anak asuh to alumni status

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Task 3.1.2: Scheduler harian untuk evaluasi alumni
+Schedule::command('anak-asuh:update-alumni')->dailyAt('00:00');
